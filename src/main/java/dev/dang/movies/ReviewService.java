baa23ddcbff2872e182e7b6c044648d53335723f
dang-ng <nguyen.hai.dang@icloud.com>
2023-02-19 14:18:40 +0700

    finish complete movie API

---
+++ src/main/java/dev/dang/movies/ReviewService.java
@@ -0,0 +1,22 @@
+package dev.dang.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    @Autowired
+    private ReviewRepository repository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    public Review createReview(String body, String imdbId){
+        Review review = repository.insert(new Review(body));
+        mongoTemplate.update(Movie.class).matching(Criteria.where("imdbId").is(imdbId)).apply(new Update().push("reviewIds").value(review)).first();
+
+        return review;
+    }
+    
+}
